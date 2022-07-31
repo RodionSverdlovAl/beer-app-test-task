a3286b1fed03158fe5e7cec20231114b2d452598
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-07-31 23:49:38 +0300

    add styles for beer page

--- pages/[id].js
+++ pages/[id].js
@@ -36,7 +36,24 @@ const Beer = ({beer}) =>{
     },[])
     return(
         <main>
-          <div>Название: {beer[0].name}</div>
+          <h1>Название: {beer[0].name}</h1>
+          <div className="beer-page">
+            <div className="beer-image">
+              <img src={beer[0].image_url}></img>
+            </div>
+            <div className="description">
+                <p>{beer[0].tagline}</p>
+                <p>abv: {beer[0].abv}</p>
+                <p>Description: {beer[0].description}</p>
+
+               
+                <ul>
+                <p>Food pairing:</p>
+                  {beer[0].food_pairing.map(li=> <li>{li}</li>)}
+                </ul>
+            </div>
+          </div>
+          
         </main>
         
     )
